75d0d1359e62618eba00f8bd8c7e69ea3814c57b
sheru007 <jnvh1233sheru@gmail.com>
2024-04-11 01:06:59 +0530

    all section done

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import HeroSection from './components/HeroSection';
 import Skills from './components/Skills';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import ProjectDetails from './components/ProjectDetails';
+import Contact from './components/Contact';
 
 
 const Body = styled.div`
@@ -34,9 +38,15 @@ function App() {
             <Skills />
             <Experience />
           </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
           <Wrapper>
             <Education />
+            <Contact />
           </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
         </Body>
       </Router>
     </ThemeProvider>
