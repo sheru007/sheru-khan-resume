042bca25cf99e795199d75ba5a6cdfbb96da4735
sheru007 <jnvh1233sheru@gmail.com>
2024-04-20 11:36:13 +0530

    resume link updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sheru007",
-  resume: "https://drive.google.com/file/d/1JzOQp_TaUHfSIWyCnBZYSITkGXOJNSLL/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1dmw0Q_FSJV3gCmyuroOwE1YmqMXHrrtu/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sheru007/",
   leetcode: "https://leetcode.com/sheru/",
   twitter: "#",
