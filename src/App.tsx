8db0821509ad07286b2b8a99c50055aeeb270c63
sheru007 <jnvh1233sheru@gmail.com>
2024-04-20 09:55:16 +0530

    deploy again

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const Body = styled.div`
   width: 100%;
 `
 
+
 const Wrapper = styled.div`
   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
